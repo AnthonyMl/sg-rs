6d35867dabaaa582056b35e5fb4bade7bc58c5ac
AnthonyMl <mladenov.anthony@gmail.com>
2016-04-03 09:56:55 -0400

    Refactor Model loading for conciseness and immutability

--- src/model.rs
+++ src/model.rs
@@ -18,44 +18,36 @@ impl Model {
 	pub fn new(context: &GlutinFacade) -> Model {
 		const MODEL_PATH_STRING: &'static str = "./data/buddha.obj";
 		let path = Path::new(MODEL_PATH_STRING);
+		let error_message = &format!("Unable to load Model({})", MODEL_PATH_STRING);
 
-		let model = match tobj::load_obj(path) {
-			Ok((ref mut models, _)) => {
-				match models.pop() {
-					Some(m) => m,
-					None => { panic!("Unable to load Model({})", MODEL_PATH_STRING) },
-				}
-			},
-			Err(_) => { panic!("Unable to load Model({})", MODEL_PATH_STRING) },
-		};
+		let model: tobj::Model = tobj::load_obj(path).expect(error_message).0.pop().expect(error_message);
 
-		let normals = if model.mesh.normals.is_empty() {
-			Model::calculate_normals(&model.mesh.positions, &model.mesh.indices)
+		let normals: Vec<[f32;3]> = if model.mesh.normals.is_empty() {
+			let (face_normals, associated_tris) = Model::calculate_face_normals_and_associated_triangles(model.mesh.positions.as_slice(), model.mesh.indices.as_slice());
+
+			(0..(model.mesh.positions.len()/3)).map(move |index| {
+				let n = associated_tris[index].iter().fold(
+					Vector3::new(0f32, 0f32, 0f32),
+					|acc, &i| { acc + face_normals[i as usize] }
+				).normalize();
+
+				[n.x, n.y, n.z]
+			}).into_iter().collect()
 		} else {
 			model.mesh.normals.chunks(3).map(|v| [v[0], v[1], v[2]]).collect()
 		};
 
-		// TODO: can we do a Vec::from_raw_parts or a transmute here
-		//
-		let mut vertices: Vec<Vertex3> = Vec::with_capacity(model.mesh.positions.len()/3);
-		let iterator = model.mesh.positions.chunks(3).zip(normals);
-
-		for (v, n) in iterator {
-			vertices.push(Vertex3 {
-				position: [v[0], v[1], v[2]],
-				normal: n,
-			});
-		}
-
-		let indices = model.mesh.indices;
+		let vertices: Vec<Vertex3> = model.mesh.positions.chunks(3).zip(normals).map(
+			|(v, n)| Vertex3{ position: [v[0], v[1], v[2]], normal: n }
+		).collect();
 
 		Model {
 			vertex_buffer: VertexBuffer::new(context, &vertices).unwrap(),
-			index_buffer: IndexBuffer::new(context, PrimitiveType::TrianglesList, &indices).unwrap(),
+			index_buffer: IndexBuffer::new(context, PrimitiveType::TrianglesList, &model.mesh.indices).unwrap(),
 		}
 	}
 
-	fn calculate_normals(vertices: &[f32], indices: &[u32]) -> Vec<[f32;3]> {
+	fn calculate_face_normals_and_associated_triangles(vertices: &[f32], indices: &[u32]) -> (Vec<Vector3<f32>>, Vec<Vec<u32>>) {
 		let mut face_normals: Vec<Vector3<f32>> = Vec::with_capacity(indices.len()/3);
 		let mut associated_tris: Vec<Vec<u32>> = (0..(vertices.len()/3)).map(|_| Vec::new()).collect();
 
@@ -75,19 +67,6 @@ impl Model {
 
 			face_normals.push(ba.cross(ca).normalize());
 		}
-
-		let mut normals = Vec::with_capacity(vertices.len());
-
-		for index in 0..(vertices.len()/3) {
-			let normal = associated_tris[index].iter().fold(
-				Vector3::new(0f32, 0f32, 0f32),
-				|acc, &i| { acc + face_normals[i as usize] }
-			);
-
-			let normal = normal.normalize();
-
-			normals.push([normal.x, normal.y, normal.z]);
-		}
-		normals
+		(face_normals, associated_tris)
 	}
 }
